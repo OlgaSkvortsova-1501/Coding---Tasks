6eb88130560bd3cd91f4e0b92a5a145331aaf067
OlgaSkvortsova-1501 <78736275+OlgaSkvortsova-1501@users.noreply.github.com>
2021-05-09 14:55:02 +0300

    Add files via upload

---
+++ task-6-circles/Cylinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_6_circles
+{
+    class Cylinder: ICylinder
+    {
+        private double _radius;
+        private double _height;
+
+        public void SetHeight(double height)
+        {
+            if (_height > 0)
+            {
+                _height = height;
+            }
+            
+                
+        }
+
+        public void SetRadius(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double Volume()
+        {
+            return _radius * _radius * _height * Math.PI;
+        }
+    }
+}
---
+++ task-6-circles/Sfera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_6_circles
+{
+    class Sfera : ISfera
+    {
+        private double _radius;
+
+        public void SetRadius(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double Volume()
+        {
+            return 4 / 3 * Math.PI * _radius * _radius * _radius;
+        }
+    }
+}
