a4a2f8ac5000df5af7de308a0352dbbb1034bd4f
OlgaSkvortsova-1501 <78736275+OlgaSkvortsova-1501@users.noreply.github.com>
2021-06-15 16:38:07 +0300

    Add files via upload

---
+++ Зарплаты в магазине/Водитель.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Зарплаты_в_магазине
+{
+    class Vodila : ALL
+    {
+
+        private double _BaseZP;
+        private double _MonProfit;
+       
+
+        public double Formula3()
+        {
+            return _BaseZP * 0.85;
+        }
+
+        public void Profit(double MonProfit)
+        {
+            _MonProfit = MonProfit;
+
+        }
+
+        public void ZP(double BaseZP)
+        {
+            _BaseZP = BaseZP;
+        }
+
+      
+    }
+}
---
+++ Зарплаты в магазине/Старший продавец.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Зарплаты_в_магазине
+{
+    class StProdaves : ALL
+    {
+        private double _MonProfit;
+        private double _BaseZP;
+
+        public double Formula1()
+        {
+          
+            return (Math.Pow(_BaseZP, 2)) * 0.15 * (Math.Pow(10,-3)) + _MonProfit * 0.1 * (Math.Pow(10, (-2)));
+        }
+
+        public void ZP(double BaseZP)
+        {
+            _BaseZP = BaseZP;
+        }
+
+        public void Profit(double MonProfit)
+        {
+            _MonProfit = MonProfit;
+
+        }
+
+       
+    }
+}
