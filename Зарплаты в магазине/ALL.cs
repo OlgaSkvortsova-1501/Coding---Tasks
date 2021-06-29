22ba7166151fbe4872c50e98572097a4311c2a39
OlgaSkvortsova-1501 <78736275+OlgaSkvortsova-1501@users.noreply.github.com>
2021-06-29 21:37:48 +0300

    Add files via upload

---
+++ Зарплаты в магазине/ALL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Зарплаты_в_магазине
+{
+    interface ALL
+    {
+        void ZP(double BaseZP);
+
+        void Profit(double MonProfit);
+    }
+
+}
---
+++ Зарплаты в магазине/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Зарплаты_в_магазине
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Добавить_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (VodilaButton.IsChecked.Value)
+            {
+
+                try
+                {
+
+                    Vodila myVodila = new Vodila();
+
+
+                    myVodila.ZP(Double.Parse(BaseZPText.Text));
+                    
+                    Driver.Text = "ЗП " + myVodila.Formula3().ToString();
+                    Driver.Foreground = Brushes.White;
+                }
+                catch (Exception ex)
+                {
+                    BaseZPText.Foreground = Brushes.Red;
+                }
+            }
+
+            if (MenedjerButton.IsChecked.Value)
+            {
+                try
+                {
+                    
+                    Menedjer myMenedjer = new Menedjer();
+                    
+                    myMenedjer.Profit(Double.Parse(MonProfitText.Text));
+                    myMenedjer.ZP(Double.Parse(BaseZPText.Text));
+                    
+                    Manager.Text = "ЗП" + myMenedjer.Formula2();
+                    Manager.Foreground = Brushes.White;
+
+                }
+                catch (Exception ex)
+                {
+                    BaseZPText.Foreground = Brushes.Red;
+                }
+
+
+            }
+
+            if (StProvadesButton.IsChecked.Value)
+            {
+
+                try
+                {
+
+                    StProdaves myStProdaves = new StProdaves();
+
+
+                    myStProdaves.Profit(Double.Parse(MonProfitText.Text));
+                    myStProdaves.ZP(Double.Parse(BaseZPText.Text));
+                    
+                    SeniorSeller.Text = "ЗП" + myStProdaves.Formula1();
+                    SeniorSeller.Foreground = Brushes.White;
+                }
+                catch (Exception ex)
+                {
+                    BaseZPText.Foreground = Brushes.Red;
+                }
+            }
+
+        }
+
+        private void Text_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (BaseZPText.Foreground != Brushes.White)
+                BaseZPText.Foreground = Brushes.White;
+        }
+    }  
+}
