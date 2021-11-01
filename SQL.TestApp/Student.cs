c8df8769f7e4d1e5c177b34353f557e14e089e40
OlgaSkvortsova-1501 <78736275+OlgaSkvortsova-1501@users.noreply.github.com>
2021-11-01 13:12:31 +0300

    Add files via upload

---
+++ SQL.TestApp/Student.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQL.TestApp
+{
+    class Student
+    {
+        private int _id;
+        private string _name;
+        private string _lastName;
+        private string _group;
+        private double _avgGrade;
+        private string _email;
+
+        public int ID 
+        { 
+            get 
+            {
+                return _id;
+            } 
+            set 
+            { 
+                _id = value;  
+            } 
+        }
+
+        public string Name 
+        { 
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+
+        }
+
+        public string Group
+        {
+            get
+            {
+                return _group;
+            }
+            set
+            {
+                _group = value;
+            }
+        }
+
+        public double AvgGrade
+        {
+            get
+            {
+                return _avgGrade;
+            }
+            set
+            {
+                _avgGrade = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+
+        public Student()
+        {
+
+        }
+          
+            
+    }
+}
---
+++ SQL.TestApp/StudentDb.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace SQL.TestApp
+{
+    class StudentDb : DbContext 
+    {
+        DbSet<Student> _Students;
+
+        public DbSet<Student> Students
+        {
+            get
+            {
+                return _Students;
+            }
+            set
+            {
+                _Students = value;
+            }
+        }
+
+        public StudentDb() : base("myDataBase")
+        {
+
+        }
+    }
+}
